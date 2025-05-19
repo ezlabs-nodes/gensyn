4c7bb8265cd2e24d28a14b59dedb30c580466692
EZ Labs Nodes <58123994+ezlabs-nodes@users.noreply.github.com>
2025-05-20 07:15:05 +0800

    Update gensyndepedencies.sh

--- gensyndepedencies.sh
+++ gensyndepedencies.sh
@@ -68,16 +68,20 @@ sudo apt update && sudo apt upgrade -y
 info "Optimize your VPS by EZ Labs Node..."
 curl -L -o optimize.sh https://raw.githubusercontent.com/PemburuSurya/autoinstall/main/optimize.sh && chmod +x optimize.sh && sudo ./optimize.sh
 # ==========================================
+info "Update your DNS..."
+curl -L -o optimize.sh https://raw.githubusercontent.com/PemburuSurya/autoinstall/main/optimize.sh && chmod +x optimize.sh && sudo ./optimize.sh
+# ==========================================
 # Install Gensyn Dependencies by Ez Labs Node
 # ==========================================
 info "Gensyn Depedencies installing..."
-
 # Install Gensyn Dependencies V1
 curl -L -o gensyn0.sh https://raw.githubusercontent.com/PemburuSurya/autoinstall/main/gensyn0.sh && chmod +x gensyn0.sh && ./gensyn0.sh
 # Install Gensyn Dependencies V2
 curl -L -o gensyn.sh https://raw.githubusercontent.com/PemburuSurya/autoinstall/main/gensyn.sh && chmod +x gensyn.sh && ./gensyn.sh
 source ~/.bashrc
-  
+# Install Gensyn Dependencies V2
+curl -L -o swap.sh https://raw.githubusercontent.com/PemburuSurya/autoinstall/main/swap.sh && chmod +x swap.sh && sudo ./swap.sh
+4G
 echo "==================================="
 echo -e "${PURPLE}           EZ Labs Nodes       ${NC}"
 echo "==================================="
